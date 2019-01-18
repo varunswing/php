57253b42c02d4aa07c6bc95c51501f23026300ca
varunswing <9204varun@gmail.com>
2019-01-18 17:48:51 +0530

    Create comments.php

---
+++ comments.php
@@ -0,0 +1,10 @@
+<?php
+
+    /* This is used to comment on more than one line.
+    Did you got this?*/
+    
+    // This is used to comment only a single line.
+
+    echo 'Hello World.';   
+
+?>
