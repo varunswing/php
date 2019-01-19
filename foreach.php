cdd62711bbd0a58b30cec7f1a6aec8c212bd5121
varunswing <9204varun@gmail.com>
2019-01-19 23:00:21 +0530

    Create foreach.php

---
+++ foreach.php
@@ -0,0 +1,13 @@
+<?php
+
+    $food = array('Healthy'=>array('Salad', 'Vegetables', 'Pasta'),'Unhealthy'=>array('Pizza', 'Ice cream'));
+
+    foreach($food as $element=> $inner_array){
+        echo '<strong>', $element.'</strong><br>';
+        
+        foreach($inner_array as $item){
+            echo $item.'<br>';
+        }
+    }
+
+?>    
