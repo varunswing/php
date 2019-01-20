773f5f70388dd2718f64b7d984f22983a7b3d855
varunswing <9204varun@gmail.com>
2019-01-21 00:02:25 +0530

    Create random-number.php

---
+++ random-number.php
@@ -0,0 +1,27 @@
+<?php
+
+    if(isset($_POST['roll'])){
+        $rand = rand(1, 6);
+
+        echo 'You rolled a '.$rand;
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="random-number.php" method="POST">
+<input type="submit" name="roll" value="Roll Dice">
+</form>
+
+</body>
+</html>
